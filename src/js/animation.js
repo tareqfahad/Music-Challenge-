204018283b1c965a60c21115253a68814b1f08e8
Tareq Alotaibi <tareqdev@protonmail.com>
2019-10-14 15:12:56 +0300

    add Animations

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,17 +1,88 @@
-function move() {
-          
-          let elem = document.getElementById("char");
-          let pos = 0;
-          let id = setInterval(frame , 0);
-          
-          function frame() {
-                    if (pos == 200) {
-                      clearInterval(id);
-                    } else {
-                      pos++;
-                      elem.style.top = pos + 'px';
-                      
-                    }
-                  }
+
+
+////////////////////////
+// REMOVE THE BUTTOMS ///
+///////////////////////
+
+function hideButtons() {
+  for (let el of document.querySelectorAll('.btn')) el.style.visibility = 'hidden';
+  
+}
+
+
+
+
+        
+
+
+function moveCenter(Speed) {
+ 
+  anime({
+    targets: '#char',
+    translateY: -200,
+    rotate: '0turn',
+    duration: Speed,
+    easing:'linear'
+  });
+  hideButtons()
+
 
 }
+
+
+function moveUP() {
+  
+
+  anime({
+    targets: '#char',
+    translateY: -340,
+    rotate: '0turn',
+    duration: 2000,
+    easing:'linear'
+  });
+
+
+  
+
+
+  hideButtons()
+  
+}
+
+
+function moveRight() {
+
+ 
+ 
+  setTimeout(function(){
+    anime({
+      targets: '#char',
+      translateX: 420,
+      rotate: '0turn',
+      duration: 1500,
+      easing:'linear'
+    });
+
+  }, 1000);
+ 
+  moveCenter(1000)
+
+}
+
+function moveLeft() {
+  
+  setTimeout(function(){
+    anime({
+      targets: '#char',
+      translateX: -400,
+      rotate: '0turn',
+      duration:1500,
+      easing:'linear'
+    });
+
+  }, 1000);
+ 
+  moveCenter(1000)
+  
+}
+
